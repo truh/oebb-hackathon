c11110d84feb2790a2737bf05c775bbee97d3567
Florian Bauer <florian@moonshiner.at>
2017-06-22 16:05:37 +0200

    message new

--- src/shared/config.js
+++ src/shared/config.js
@@ -2,7 +2,7 @@
 
 export const WEB_PORT = process.env.PORT || 8000
 export const STATIC_PATH = '/static'
-export const APP_NAME = 'Hello Appom'
+export const APP_NAME = 'New Test'
 
 export const WDS_PORT = 7000
 
