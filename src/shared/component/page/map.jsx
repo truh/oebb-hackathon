e8ab31b372a1a7abed2cfc97e2003a6adfbcb020
Jakob Klepp <jakob@klepp.email>
2017-06-23 03:19:14 +0000

    Add scrollbar to MapPage

--- src/shared/component/page/map.jsx
+++ src/shared/component/page/map.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import Helmet from 'react-helmet'
+import Slider from 'rc-slider'
 
 import TrainMap from '../train-map'
 
@@ -11,8 +12,26 @@ const center = { lat: 48.2, lng: 16.2 }
 const zoom = 11
 
 const containerStyle = {
-  height: '100%',
+  height: '90%',
 }
+
+const sliderStyle = {
+  width: 400,
+  height: '10%',
+}
+
+/* eslint-disable */
+const sliderMarks = {
+  '1': <strong>01.11.2016</strong>,
+  '6': <strong>06.11.2016</strong>,
+  '12': <strong>12.11.2016</strong>,
+  '18': <strong>18.11.2016</strong>,
+  '24': <strong>24.11.2016</strong>,
+  '30': <strong>30.11.2016</strong>,
+}
+/* eslint-enable */
+
+const sliderContainerClassName = 'footer navbar-fixed-bottom'
 
 const MapPage = () => (
   <div style={containerStyle}>
@@ -24,8 +43,18 @@ const MapPage = () => (
       ]}
     />
     <TrainMap center={center} zoom={zoom} />
+    <div style={sliderStyle} className={sliderContainerClassName}>
+      <Slider
+        min={1}
+        max={30}
+        step={1}
+        dots
+        marks={sliderMarks}
+        onAfterChange={sliderMarks}
+        defaultValue={22}
+      />
+    </div>
   </div>
-  <div class="footer navbar-fixed-bottom">
 )
 
 export default MapPage
--- src/shared/component/train-map.jsx
+++ src/shared/component/train-map.jsx
@@ -3,21 +3,12 @@ import GoogleMapReact from 'google-map-react'
 
 const mapOnChange = () => { }
 
-const trainMarkerClassName = 'train-marker'
-
-const TrainMarker = text => (
-  <div className={trainMarkerClassName}>
-    {text}
-  </div>
-)
-
 const TrainMap = prop => (
   <GoogleMapReact
     onChange={mapOnChange}
     center={prop.center}
     zoom={prop.zoom}
-  >
-  </GoogleMapReact>
+  />
 )
 
 export default TrainMap
