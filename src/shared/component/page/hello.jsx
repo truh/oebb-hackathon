1b47a4613ab8e0b5a47033fcedacfd93313da4fb
Florian Bauer <florian@moonshiner.at>
2017-06-23 04:05:42 +0200

    slider

--- src/shared/component/page/hello.jsx
+++ src/shared/component/page/hello.jsx
@@ -1,9 +1,7 @@
-// @flow
-
 import React from 'react'
 import Helmet from 'react-helmet'
 
-import Slider, { Range } from 'rc-slider'
+import Slider from 'rc-slider'
 
 import { isServer } from '../../util'
 import Message from '../../container/message'
@@ -14,6 +12,19 @@ if (!isServer) {
   require('rc-slider/assets/index.css')
 }
 
+const style = { width: 400, margin: 50 }
+const marks = {
+  1: <strong>01.11.2016</strong>,
+  6: <strong>06.11.2016</strong>,
+  12: <strong>12.11.2016</strong>,
+  18: <strong>18.11.2016</strong>,
+  24: <strong>24.11.2016</strong>,
+  30: <strong>30.11.2016</strong>,
+}
+
+function log(value) {
+  console.log(value); //eslint-disable-line
+}
 
 const title = 'Hello Page'
 
@@ -29,8 +40,18 @@ const HelloPage = () => (
     <div className="row">
       <div className="col-12">
         <h1>{title}</h1>
-        <Slider />
-        <Range />
+        <div style={style}>
+          <p>Slider</p>
+          <Slider
+            min={1}
+            max={30}
+            step={1}
+            dots
+            marks={marks}
+            onAfterChange={log}
+            defaultValue={22}
+          />
+        </div>
         <Message />
         <HelloButton />
       </div>
